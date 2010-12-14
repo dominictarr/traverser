3354f8a7ec7fa0f86837cc945ea5aaea8c832a1e
Dominic Tarr <dominic.tarr@gmail.com>
2010-12-14 18:44:28 +1300

    opps,
    it wasn't slower, I have just started using meta-test rather than expresso.
    
    that was what was slower. sync only and a/sync versions are about the same speed

--- traverser2.sync.js
+++ traverser2.sync.js
@@ -1,14 +1,12 @@
-//traverser2
+//traverser2.sync
 var inspect = require('inspect')
   , log = require('logger')
   , curry = require('curry')
   , sync = require('./iterators').sync
-  , async = require('./iterators').async
 
 module.exports = traverse
 exports.isObject = isObject
 exports.isComplex = isComplex
-
 
 function isObject (props){
   return ('object' === typeof props.value)
@@ -24,50 +22,29 @@ var complex =
 function isComplex (props){
   return complex[typeof props.value]
 }
-function defaultLeaf(p){
-  return p.value
-}
-function defaultBranch (p){
-  return p.iterate()
-}
-function defaultLeafAsync(p,next){
-  next(p.value)
-}
-function defaultBranchAsync (p,next){
-  //log('DEFAULT BRANCH ASYNC')
-  p.iterate(next)
-}
 
-function traverse (object,opts,done){
+function traverse (object,opts){
 
   if('function' == typeof opts)
-    opts = { each: opts
-           , done: done }
-
-  opts.async = !!(opts.done)//async mode if done is defined.
+    opts = {each: opts}
 
   if (opts.each)
     opts.leaf = opts.branch = opts.each
   if(!opts.leaf)
-    opts.leaf = opts.async ? defaultLeafAsync : defaultLeaf
+    opts.leaf = function (p){return p.value}
   if(!opts.branch)
-    opts.branch = opts.async ? defaultBranchAsync : defaultBranch
+    opts.branch = function (p){return p.iterate()}
 
   if(!opts.isBranch)
     opts.isBranch = exports.isObject
 
-  var cont = opts.done ? async : sync
-
-  if(!opts.iterator)
-    opts.iterator = 'map'
-
   if('string' == typeof opts.iterator){
     var s = opts.iterator
-    opts.iterator = cont[s]
+    opts.iterator = sync[s]
     
     if (!opts.iterator)
       throw new Error('\'' + s + '\' is not the name of a traverse iterator.'
-        + ' try one of [' + Object.keys(cont) + ']')
+        + ' try one of [' + Object.keys(sync) + ']')
     }
 
   var props = 
@@ -80,12 +57,12 @@ function traverse (object,opts,done){
         , path: [] 
         , seen: []
         , ancestors: []
-        , iterate: curry([opts.iterator],iterate)
+        , iterate: curry(iterate,[opts.iterator])
         }
 
   //setup iterator functions -- DIFFERENT IF ASYNC
-  Object.keys(cont).forEach(function(key){
-    var func = cont[key]
+  Object.keys(sync).forEach(function(key){
+    var func = sync[key]
     props[key] = curry([func],iterate)
   })
 
@@ -104,50 +81,35 @@ function traverse (object,opts,done){
     props.repeated = refs
   }
         
-  function iterate(iterator,done){
+  function iterate(iterator){
     var _parent = props.parent
       , _key = props.key
       , _value = props.value
-      , _index = props.index
-      , _referenced = props.referenced
-      , r
-    //log('DONE()',done)
+      , returned 
+
     props.ancestors.push(props.value)
     props.parent = props.value
-    props.next = c
-    r = iterator(props.value,makeCall,c)
-    //seperate this function for async
-    if(!opts.async) return c(r)
-    function c(r){
-      //log('teardown branch ',r)
-      
-      props.key = _key
-      props.value = _value
-      props.parent = _parent
-      props.index = _index
-      if(opts.pre)
-        props.referenced = _referenced
+    returned = iterator(props.value,makeCall)
 
-      props.ancestors.pop()
-      if(opts.async) done(r)
-      return r //returned will be ignored if async
-    }
+    props.key = _key
+    props.value = _value
+    props.parent = _parent
+
+    props.ancestors.pop()
+    return returned//returned will be ignored if async
   }
 
-  function makeCall(value,key,next){//next func here if async.
-    var r, index
-    //using immutable objects would simplify this greatly, 
-    //because I could not have to teardown...
-    //maybe. would have to not depend on closures.
-    if(key !== null)
+  function makeCall(value,key){//next func here if async.
+    var r
+
+    if(key !== null)//if move all this into iterate, won't need this check, because it won't be called on root.
       props.path.push(key)
     props.key = key
     props.value = value
-    if(opts.async)
-      props.next = c
+
 
     if(opts.isBranch(props)){
-      index = 
+      var index = 
         { seen: props.seen.indexOf(props.value)
         , ancestors: props.ancestors.indexOf(props.value) }
         
@@ -162,19 +124,18 @@ function traverse (object,opts,done){
       ;(props.reference = (-1 !== index.seen)) 
         || props.seen.push(value)
 
-      r = opts.branch(props,c)
+      r = opts.branch(props)//,next if async
     } else {
-      r = opts.leaf(props,c)
+      r = opts.leaf(props)
     }
-    
-    if(!opts.async) return c(r) //finish up, if sync
-    function c (r){
-      if(key !== null)
-        props.path.pop()
-      if(opts.async) next(r)
-      return r
-    }
+    //finish up, if sync
+    return (function (){
+    if(key !== null)
+      props.path.pop()
+    return r
+    })()
   }
   
- return makeCall(object,null,opts.done)
+ return makeCall(object,null)
 }
+
